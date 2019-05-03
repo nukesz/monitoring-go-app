5f82d60ea6295ab24b0938b936a149d70aa03804
Norbert Benczúr <nbe@vizrt.com>
2019-05-03 20:58:58 +0200

    Added prometheus metric endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nukesz/monitoring-go-app
 
 go 1.12
+
+require github.com/prometheus/client_golang v0.9.2
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	log.Println("Hello World")
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
 }
